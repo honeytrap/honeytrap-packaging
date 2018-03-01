7dcd2cf4f8e171bc0bfce0dfab0308ae9aa1a50d
Remco Verhoef <remco@dutchcoders.io>
2018-03-01 23:25:28 +0100

    debian: added versioning

--- debian/build-honeytrap-agent.sh
+++ debian/build-honeytrap-agent.sh
@@ -7,13 +7,20 @@ mkdir -p $DEST
 cd $DEST
 git clone https://github.com/honeytrap/honeytrap-agent .
 
+COMMIT=$(git rev-parse --short HEAD)
+echo $COMMIT
+
+DATE=$(git log -1 --format=%cd --date=format:'%Y%m%d')
+echo $DATE
+
 LDFLAGS=$(go run scripts/gen-ldflags.go)
-GOOS=linux
-GOARCH=amd64 
 go build -a -ldflags "$LDFLAGS -extldflags \"-static\"" -o /go/bin/app
 
 /go/bin/app --version
 
 cd /build
 cp /go/bin/app honeytrap-agent/usr/bin/honeytrap-agent
+
+cat honeytrap-agent/DEBIAN/control.template | sed -e s/#DATE#/$DATE/ | sed -e s/#COMMIT#/$COMMIT/ > honeytrap-agent/debian/control
+
 dpkg-deb --build honeytrap-agent 
--- debian/build-honeytrap.sh
+++ debian/build-honeytrap.sh
@@ -7,6 +7,12 @@ mkdir -p $DEST
 cd $DEST
 git clone https://github.com/honeytrap/honeytrap .
 
+COMMIT=$(git rev-parse --short HEAD)
+echo $COMMIT
+
+DATE=$(git log -1 --format=%cd --date=format:'%Y%m%d')
+echo $DATE
+
 LDFLAGS=$(go run scripts/gen-ldflags.go)
 go build -a -ldflags "$LDFLAGS -extldflags \"-static\"" -o /go/bin/app
 
@@ -14,4 +20,7 @@ go build -a -ldflags "$LDFLAGS -extldflags \"-static\"" -o /go/bin/app
 
 cd /build
 cp /go/bin/app honeytrap/usr/bin/honeytrap
+
+cat honeytrap/DEBIAN/control.template | sed -e s/#DATE#/$DATE/ | sed -e s/#COMMIT#/$COMMIT/ > honeytrap/debian/control
+
 dpkg-deb --build honeytrap
